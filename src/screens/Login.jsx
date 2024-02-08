c46f54a020eeb0aea646ccc6427c37c6bbebd772
tahiee <niazi.tahirkhan15@gmail.com>
2024-02-08 18:14:41 +0500

    bg

--- src/screens/Login.jsx
+++ src/screens/Login.jsx
@@ -7,7 +7,6 @@ import TextField from "@mui/material/TextField";
 import FormControlLabel from "@mui/material/FormControlLabel";
 import Checkbox from "@mui/material/Checkbox";
 import Link from "@mui/material/Link";
-import Paper from "@mui/material/Paper";
 import Box from "@mui/material/Box";
 import Grid from "@mui/material/Grid";
 import LockOutlinedIcon from "@mui/icons-material/LockOutlined";
@@ -15,8 +14,7 @@ import Typography from "@mui/material/Typography";
 import { createTheme, ThemeProvider } from "@mui/material/styles";
 import { useNavigate } from "react-router-dom";
 import { loginUser, signUpUser } from "../firebase/firebasemethod.js";
-import { AltRoute } from "@mui/icons-material";
-import backgroundImage1 from "../../img/bg-01.jpg";
+// import backgroundImage1 from "../../img/bg-01.jpg";
 import backgroundVideo1 from "../../img/8bd7760b-30da-4964-b8bf-75089cb53938.mp4";
 
 function Copyright(props) {
